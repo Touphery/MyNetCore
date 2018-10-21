5b2619cc86f804d3ea80cda197adcefb9f94f011
Touphery <tengsn@126.com>
2018-10-21 08:57:32 +0800

    My Test Core Console App

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine("My Test Core Console");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
